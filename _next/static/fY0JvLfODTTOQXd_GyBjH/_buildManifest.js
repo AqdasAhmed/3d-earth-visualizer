f9d9df9312552f060529070aada179bb5afef479
AqdasAhmed <144926320+AqdasAhmed@users.noreply.github.com>
2025-11-26 09:33:00 +0530

    Updates

---
+++ _next/static/fY0JvLfODTTOQXd_GyBjH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/3d-earth-visualizer//_next/:path+",
+        "destination": "/3d-earth-visualizer/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
